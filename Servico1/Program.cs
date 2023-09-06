554337fde030c8b84d62b515edef80f5bd09012b
vinicius z <vinizacc@gmail.com>
2023-09-06 14:21:20 -0300

    thrid commit

--- Servico1/Program.cs
+++ Servico1/Program.cs
@@ -14,7 +14,7 @@ namespace Servico1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormAcessoVendas());
+            Application.Run(new Login());
         }
     }
 }
